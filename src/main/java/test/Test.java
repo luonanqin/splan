5edbf3526ef827af4e7ae7821d4585ac5e70bcd9
nanqin.luo <nanqin.luo@dianping.com>
2023-02-09 02:42:13 +0800

    update

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,7 +1,11 @@
 package test;
 
+import com.google.common.collect.Lists;
 import util.BaseUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,9 +13,38 @@ import java.util.Map;
  */
 public class Test {
 
-    public static void main(String[] args) throws Exception{
-        String market = "XNAS";
-        Map<String, String> openMap = BaseUtils.getOpenData(market);
+    public static void main(String[] args) throws Exception {
+        //        List<String> markets = Lists.newArrayList("XNAS-ADRC","XNYS-ADRC");
+        //        List<String> markets = Lists.newArrayList("XNAS","XNYS");
+        List<String> markets = Lists.newArrayList("XNAS");
 
+        int size = 100;
+        int sum = 0;
+        long i = 1000 * 3600 * 24;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        long nowTime = format.parse("2023-01-01").getTime();
+        long _2000 = format.parse("2000-01-01").getTime();
+
+        for (String market : markets) {
+            Map<String, String> openMap = BaseUtils.getOpenData(market);
+            for (String stock : openMap.keySet()) {
+                String open = openMap.get(stock);
+                Date openDate = format.parse(open);
+                long openTime = openDate.getTime();
+                if (openTime < _2000) {
+                    openTime = _2000;
+                }
+
+                int count = Math.abs((int) ((nowTime - openTime) / i));
+                //                System.out.println(stock + " " + count);
+                sum += count;
+                size--;
+                if (size < 0) {
+//                    break;
+                }
+            }
+            System.out.println(market + " " + sum);
+        }
     }
 }
