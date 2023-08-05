a8fdfa94b4cf1849e960bbfc0d6d9b3d9fad214b
高明 <luonanqin@xiaohongshu.com>
2023-08-05 18:16:32 +0800

    update

--- src/main/java/luonq/strategy/Strategy6_1.java
+++ src/main/java/luonq/strategy/Strategy6_1.java
@@ -21,62 +21,66 @@ import java.util.Map;
  */
 public class Strategy6_1 {
 
-    public static String TEST_STOCK = "";
+    public static String TEST_STOCK = "SA";
     public static String TEST_DATE = "";
 
 
     public static void main(String[] args) throws Exception {
         Map<String, String> fileMap = BaseUtils.getFileMap(Constants.TEST_PATH + "strategy6");
         List<Double> ratioRange = Lists.newArrayList(0.5, 0.6, 0.7, 0.8, 0.9);
-        for (Double ratio : ratioRange) {
-            double successCount = 0, failedCount = 0;
+        //        for (Double ratio : ratioRange) {
+        double ratio = 0.6d;
+        double successCount = 0, failedCount = 0;
 
-            for (String stock : fileMap.keySet()) {
-                String file = fileMap.get(stock);
-                if (StringUtils.isBlank(file)) {
+        for (String stock : fileMap.keySet()) {
+            String file = fileMap.get(stock);
+            if (StringUtils.isBlank(file)) {
+                continue;
+            }
+            if (StringUtils.isNotBlank(TEST_STOCK) && !stock.equals(TEST_STOCK)) {
+                continue;
+            }
+
+            //                System.out.println(stock);
+            List<String> dataList = BaseUtils.readFile(file);
+            for (int i = 1; i < dataList.size() - 1; i++) {
+                String[] split = dataList.get(i).split(",");
+                String date = split[0];
+                if (!date.endsWith("2022")) {
                     continue;
                 }
-                if (StringUtils.isNotBlank(TEST_STOCK) && !stock.equals(TEST_STOCK)) {
-                    continue;
-                }
+                double close = Double.parseDouble(split[2]);
+                double nextClose = Double.parseDouble(split[5]);
+                double lossRatio = Double.parseDouble(split[12]);
+                double gainRatio = Double.parseDouble(split[13]);
 
-//                System.out.println(stock);
-                List<String> dataList = BaseUtils.readFile(file);
-                for (int i = 1; i < dataList.size() - 1; i++) {
-                    String[] split = dataList.get(i).split(",");
-                    String date = split[0];
-                    double close = Double.parseDouble(split[2]);
-                    double nextClose = Double.parseDouble(split[5]);
-                    double lossRatio = Double.parseDouble(split[12]);
-                    double gainRatio = Double.parseDouble(split[13]);
+                boolean loss = Boolean.parseBoolean(split[14]);
+                boolean gain = Boolean.parseBoolean(split[15]);
+                boolean prevLoss = Boolean.parseBoolean(split[16]);
+                boolean lowLtPrevLow = Boolean.parseBoolean(split[19]);
+                boolean highLtPrevOpen = Boolean.parseBoolean(split[20]);
+                //                    boolean gainRatioRes = gain && gainRatio > ratio;
+                boolean lossRatioRes = loss && lossRatio > ratio;
 
-                    boolean loss = Boolean.parseBoolean(split[14]);
-                    boolean gain = Boolean.parseBoolean(split[15]);
-                    boolean prevLoss = Boolean.parseBoolean(split[16]);
-                    boolean lowLtPrevLow = Boolean.parseBoolean(split[19]);
-                    boolean highLtPrevOpen = Boolean.parseBoolean(split[20]);
-                    boolean gainRatioRes = gain && gainRatio > ratio;
-                    boolean lossRatioRes = loss && lossRatio > ratio;
+//                boolean lossRatioRes = true;
+                boolean gainRatioRes = false;
+                //                gain = true;
+                //                lowLtPrevLow = true;
+//                                highLtPrevOpen = true;
 
-                    lossRatioRes = true;
-                    gainRatioRes = true;
-                    //                gain = true;
-                    //                lowLtPrevLow = true;
-                    //                highLtPrevOpen = true;
-
-                    if ((gainRatioRes || lossRatioRes) &&
-                      lowLtPrevLow && prevLoss && highLtPrevOpen) {
-                        boolean success = nextClose > close;
-                        if (success) {
-                            successCount++;
-                        } else {
-                            failedCount++;
-                        }
-                        //                        System.out.println(date + " " + success);
+                if ((gainRatioRes || lossRatioRes) &&
+                  lowLtPrevLow && prevLoss && highLtPrevOpen) {
+                    boolean success = nextClose > close;
+                    if (success) {
+                        successCount++;
+                    } else {
+                        failedCount++;
                     }
+                    System.out.println(stock + " " + date + " " + success);
                 }
             }
-            System.out.println("ratio=" + ratio + ", successCount=" + successCount + ", failedCount=" + failedCount + ", rate=" + successCount / (successCount + failedCount));
         }
+        //            System.out.println("ratio=" + ratio + ", successCount=" + successCount + ", failedCount=" + failedCount + ", rate=" + successCount / (successCount + failedCount));
+        //        }
     }
 }
