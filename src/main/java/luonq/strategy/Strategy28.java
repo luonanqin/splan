05bf716b507f6d6d0bf9eb046d493b3eea4edd3f
nanqin.luo <nanqin.luo@dianping.com>
2024-05-15 17:07:01 +0800

    update

--- src/main/java/luonq/strategy/Strategy28.java
+++ src/main/java/luonq/strategy/Strategy28.java
@@ -67,7 +67,7 @@ public class Strategy28 {
           + "underlying_ticker=%s&expired=true&order=desc&limit=10&sort=expiration_date&expiration_date.lte=%s&expiration_date.gt=%s&strike_price.lte=%d&stike_price.gte=%d"
           + "&apiKey=Ea9FNNIdlWnVnGcoTpZsOWuCWEB3JAqY", code, upDate, date, upPrice, downPrice);
 
-        System.out.println(url);
+//        System.out.println(url);
 
         HttpGet get = new HttpGet(url);
         try {
@@ -82,7 +82,7 @@ public class Strategy28 {
 
             List<OptionContracts> results = resp.getResults();
             List<String> tickerList = results.stream().map(OptionContracts::getTicker).collect(Collectors.toList());
-            System.out.println(tickerList);
+//            System.out.println(tickerList);
 
             return tickerList;
         } finally {
@@ -174,6 +174,117 @@ public class Strategy28 {
         }
     }
 
+    public static List<String> buildTestData() {
+        List<String> dataList = Lists.newArrayList();
+        dataList.add("01/04/2024	MBLY	28.33");
+        dataList.add("01/05/2024	AGL	7.75");
+        dataList.add("01/08/2024	DADA	2.14");
+        dataList.add("01/08/2024	DADA	2.14");
+        dataList.add("01/09/2024	GRFS	7.42");
+        dataList.add("01/10/2024	CHWY	19.8");
+        dataList.add("01/11/2024	GRFS	8.58");
+        dataList.add("01/12/2024	GRFS	7.28");
+        dataList.add("01/16/2024	WIT	5.83");
+        dataList.add("01/17/2024	PHUN	0.1613");
+        dataList.add("01/18/2024	PLUG	2.3");
+        dataList.add("01/19/2024	IRBT	16.91");
+        dataList.add("01/22/2024	ADM	56.88");
+        dataList.add("01/23/2024	INO	0.61");
+        dataList.add("01/24/2024	DD	64.45");
+        dataList.add("01/25/2024	COLB	20.69");
+        dataList.add("01/26/2024	HUBG	46.8");
+        dataList.add("01/29/2024	IRBT	14.07");
+        dataList.add("01/30/2024	CALX	33.49");
+        dataList.add("01/31/2024	NYCB	5.96");
+        dataList.add("02/01/2024	PSNY	1.8");
+        dataList.add("02/02/2024	EXPO	72.26");
+        dataList.add("02/05/2024	APD	227");
+        dataList.add("02/06/2024	CCK	70.84");
+        dataList.add("02/07/2024	SNAP	12.03");
+        dataList.add("02/08/2024	CENTA	33.28");
+        dataList.add("02/09/2024	PLCE	8.5");
+        dataList.add("02/12/2024	BIG	4.25");
+        dataList.add("02/13/2024	WCC	152");
+        dataList.add("02/14/2024	QDEL	46.27");
+        dataList.add("02/15/2024	AUPH	6");
+        dataList.add("02/16/2024	COO	93.24");
+        dataList.add("02/20/2024	NNOX	10.9");
+        dataList.add("02/21/2024	AMPL	9.22");
+        dataList.add("02/22/2024	GSHD	60.57");
+        dataList.add("02/23/2024	WMT	58.6967");
+        dataList.add("02/26/2024	PHUN	0.175");
+        dataList.add("02/27/2024	TWKS	3.2");
+        dataList.add("02/28/2024	EB	6.17");
+        dataList.add("02/29/2024	CC	18");
+        dataList.add("03/01/2024	NYCB	3.45");
+        dataList.add("03/04/2024	SABR	2");
+        dataList.add("03/05/2024	GTLB	60");
+        dataList.add("03/06/2024	THO	105.945");
+        dataList.add("03/07/2024	CDMO	6.05");
+        dataList.add("03/08/2024	PBR	14.59");
+        dataList.add("03/11/2024	EQT	34.9");
+        dataList.add("03/12/2024	ACAD	19.49");
+        dataList.add("03/13/2024	SOS	1.52");
+        dataList.add("03/14/2024	MOMO	5.9");
+        dataList.add("03/15/2024	EVCM	6.9");
+        dataList.add("03/18/2024	SAIC	118.54");
+        dataList.add("03/19/2024	DLO	15.61");
+        dataList.add("03/20/2024	SIG	90");
+        dataList.add("03/21/2024	ASO	61.86");
+        dataList.add("03/22/2024	LULU	416.25");
+        dataList.add("03/25/2024	SIGA	8.11");
+        dataList.add("03/26/2024	CDLX	15.74");
+        dataList.add("03/27/2024	ODFL	219.135");
+        dataList.add("03/28/2024	MLKN	23.9");
+        dataList.add("04/01/2024	MMM	91.05");
+        dataList.add("04/02/2024	GOEV	2.4801");
+        dataList.add("04/03/2024	ULTA	469.57");
+        dataList.add("04/04/2024	LW	88.53");
+        dataList.add("04/05/2024	ATUS	2.38");
+        dataList.add("04/08/2024	SUPN	30.58");
+        dataList.add("04/09/2024	TLRY	2.12");
+        dataList.add("04/10/2024	SOUN	4.24");
+        dataList.add("04/11/2024	KMX	73.38");
+        dataList.add("04/12/2024	ANET	280.27");
+        dataList.add("04/15/2024	PSNY	1.35");
+        dataList.add("04/16/2024	PACB	1.95");
+        dataList.add("04/17/2024	SAGE	12.96");
+        dataList.add("04/18/2024	LAC	4.84");
+        dataList.add("04/19/2024	NFLX	567.88");
+        dataList.add("04/22/2024	CNHI	11.3");
+        dataList.add("04/23/2024	JBLU	6.11");
+        dataList.add("04/24/2024	EVR	176.4");
+        dataList.add("04/25/2024	META	421.4");
+        dataList.add("04/26/2024	SAIA	452.09");
+        dataList.add("04/29/2024	DB	16.14");
+        dataList.add("04/30/2024	MED	26.6");
+        dataList.add("05/01/2024	LEG	12.1");
+        dataList.add("05/02/2024	FSLY	8.15");
+        dataList.add("05/03/2024	SPT	33.99");
+        dataList.add("05/06/2024	BOWL	10.62");
+        dataList.add("05/07/2024	JELD	15.2");
+        dataList.add("05/08/2024	DV	18.67");
+        dataList.add("05/09/2024	FWRD	12.87");
+        dataList.add("05/10/2024	MVIS	1.15");
+
+        List<String> result = Lists.newArrayList();
+        for (String str : dataList) {
+            String[] split = str.split("\t");
+            String date = split[0];
+            date = BaseUtils.formatDate(date);
+
+            result.add(date + "\t" + split[1] + "\t" + split[2]);
+        }
+
+        return result;
+    }
+
+    public static String getOptionPutCode(String optionCallCode) {
+        int c_index = optionCallCode.lastIndexOf("C");
+        StringBuffer sb = new StringBuffer(optionCallCode);
+        return sb.replace(c_index, c_index + 1, "P").toString();
+    }
+
     public static void main(String[] args) throws Exception {
         ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.apache.http").setLevel(Level.INFO);
         ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("httpclient.wire").setLevel(Level.INFO);
@@ -182,31 +293,39 @@ public class Strategy28 {
 
         //        getOptionCode("AGL", 7.75, "2024-01-05");
         //        getOptionCode("DADA", 2.14, "2024-01-08");
-        List<String> codeList = Lists.newArrayList();
-        codeList.add("O:DADA240119C00010000");
-        codeList.add("O:DADA240119C00007500");
-        codeList.add("O:DADA240119C00005000");
-        codeList.add("O:DADA240119C00002500");
-        codeList.add("O:DADA240119C00002000");
-        codeList.add("O:DADA240119C00001500");
-        codeList.add("O:DADA240119C00001000");
-        codeList.add("O:DADA240119C00000500");
-        codeList.add("O:DADA240119P00010000");
-        codeList.add("O:DADA240119P00007500");
-        codeList.add("O:DADA240119P00005000");
-        codeList.add("O:DADA240119P00002500");
-        codeList.add("O:DADA240119P00002000");
-        codeList.add("O:DADA240119P00001500");
-        codeList.add("O:DADA240119P00001000");
-        codeList.add("O:DADA240119P00000500");
+        //        List<String> codeList = Lists.newArrayList();
+        //        codeList.add("O:DADA240119C00010000");
+        //        codeList.add("O:DADA240119C00007500");
+        //        codeList.add("O:DADA240119C00005000");
+        //        codeList.add("O:DADA240119C00002500");
+        //        codeList.add("O:DADA240119C00002000");
+        //        codeList.add("O:DADA240119C00001500");
+        //        codeList.add("O:DADA240119C00001000");
+        //        codeList.add("O:DADA240119C00000500");
+        //        codeList.add("O:DADA240119P00010000");
+        //        codeList.add("O:DADA240119P00007500");
+        //        codeList.add("O:DADA240119P00005000");
+        //        codeList.add("O:DADA240119P00002500");
+        //        codeList.add("O:DADA240119P00002000");
+        //        codeList.add("O:DADA240119P00001500");
+        //        codeList.add("O:DADA240119P00001000");
+        //        codeList.add("O:DADA240119P00000500");
 
-        String code = "O:DADA240119C00001000";
-        int c_index = code.lastIndexOf("C");
-        StringBuffer sb = new StringBuffer(code);
-        sb = sb.replace(c_index, c_index + 1, "P");
-        System.out.println(sb);
+        //        getOptionQuote(codeList, "2024-01-08");
 
-//        getOptionQuote(codeList, "2024-01-08");
+        List<String> dataList = buildTestData();
+        for (String data : dataList) {
+            String[] split = data.split("\t");
+            String date = split[0];
+            String code = split[1];
+            Double price = Double.valueOf(split[2]);
+            List<String> optionCallCode = getOptionCode(code, price, date);
+            List<String> optionPutCode = optionCallCode.stream().map(c -> getOptionPutCode(c)).collect(Collectors.toList());
+
+            getOptionQuote(optionCallCode, date);
+            getOptionQuote(optionPutCode, date);
+        }
+
         cachedThread.shutdown();
     }
 }
