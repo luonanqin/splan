1fd2ef5e94ce4247152bf11c6df5f5ff7a4b90c6
nanqin.luo <nanqin.luo@dianping.com>
2025-01-10 18:20:32 +0800

    update

--- src/main/java/luonq/strategy/Strategy40.java
+++ src/main/java/luonq/strategy/Strategy40.java
@@ -1,6 +1,8 @@
 package luonq.strategy;
 
+import bean.OptionDaily;
 import bean.StockKLine;
+import com.google.common.collect.Lists;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import util.BaseUtils;
 import util.Constants;
@@ -9,15 +11,26 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static util.Constants.DB_DATE_FORMATTER;
+
+/**
+ * 1.获取每周最后一天的开盘涨幅绝对值，计算其值有几个标准差，从大到小倒排并取前十
+ * 2.获取每周前3或4天的期权成交量，计算其标准差
+ * 3.获取每周前3或4天的开盘涨跌幅，计算其绝对值的标准差
+ */
 public class Strategy40 {
 
     public static void main(String[] args) throws Exception {
         Map<String, String> fileMap = BaseUtils.getFileMap(Constants.HIS_BASE_PATH + "2024/dailyKLine");
 
-        for (String stock : fileMap.keySet()) {
+        Set<String> optionStocks = BaseUtils.getPennyOptionStock();
+
+        for (String stock : optionStocks) {
+            //            Map<String, Map<String, OptionDaily>> optionDailyMap = BaseUtils.loadOptionDailyMap(stock);
             String path = fileMap.get(stock);
             List<StockKLine> stockKLines = BaseUtils.loadDataToKline(path, 2024, 2023);
             if (stockKLines.size() < 10) {
@@ -26,30 +39,54 @@ public class Strategy40 {
 
             Collections.reverse(stockKLines);
             Map<String, StockKLine> stockMap = stockKLines.stream().collect(Collectors.toMap(StockKLine::getFormatDate, Function.identity()));
+            int i = 0;
+            String startDate = null;
+            double lastClose = 0d;
+            for (; i < stockKLines.size(); i++) {
+                String curDate = stockKLines.get(i).getFormatDate();
+                LocalDate curLd = LocalDate.parse(curDate, DB_DATE_FORMATTER);
+                int cur = curLd.getDayOfWeek().getValue();
 
-            for (int i = 0; i < stockKLines.size(); i++) {
-                String mon = stockKLines.get(i).getFormatDate();
-                LocalDate ld = LocalDate.parse(mon, Constants.DB_DATE_FORMATTER);
+                String nextDate = stockKLines.get(i + 1).getFormatDate();
+                LocalDate nextLd = LocalDate.parse(nextDate, DB_DATE_FORMATTER);
+                int next = nextLd.getDayOfWeek().getValue();
 
-                int dayOfWeek = ld.getDayOfWeek().getValue();
-                if (dayOfWeek != 1) {
-                    continue;
-                }
-                if (i + 2 >= stockKLines.size()) {
+                if (next < cur) {
+                    i++;
+                    startDate = nextDate;
+                    lastClose = stockKLines.get(i).getClose();
                     break;
                 }
-                String tue = stockKLines.get(i + 1).getFormatDate();
-                String wed = stockKLines.get(i + 2).getFormatDate();
+            }
 
-                StockKLine monS = stockMap.get(mon);
-                StockKLine tueS = stockMap.get(tue);
-                StockKLine wedS = stockMap.get(wed);
-                double monV = monS.getVolume().doubleValue();
-                double tueV = tueS.getVolume().doubleValue();
-                double wedV = wedS.getVolume().doubleValue();
+            LocalDate start = LocalDate.parse(startDate, DB_DATE_FORMATTER);
+            while (true) {
+                if (start.getYear() > 2024) {
+                    break;
+                }
+                List<StockKLine> list = Lists.newArrayList();
+                String _1 = start.format(DB_DATE_FORMATTER);
+                while (true) {
+                    if (start.getDayOfWeek().getValue() >= 7) {
+                        break;
+                    }
+                    StockKLine kLine = stockMap.get(_1);
+                    start = start.plusDays(1);
+                    if (kLine != null) {
+                        list.add(kLine);
+                    }
+                }
+
+                double[] dataArray = new double[list.size() - 1];
+
+                for (int j = 0; j < list.size(); j++) {
+                    double diff = (list.get(j).getOpen() - lastClose) / lastClose;
+                    dataArray[j] = Math.abs(diff);
+                    lastClose = list.get(j).getClose();
+                }
 
                 StandardDeviation stdev = new StandardDeviation();
-                double stdevV = stdev.evaluate(new double[] { monV, tueV, wedV });
+                double stdevV = stdev.evaluate(dataArray);
                 System.out.println(stdevV);
             }
         }
